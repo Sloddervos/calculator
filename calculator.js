c7eedd0619c6d8d06cdf452aaf08713bfc021114
Oscar <oscar.bailey95@gmail.com>
2022-08-21 13:38:37 +0100

    Testing

---
+++ calculator.js
@@ -0,0 +1,16 @@
+const add = (a, b) => a + b;
+
+const subtract = (a,b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
+
+const operate = (operator, a, b) => {
+return operator === '+' ? add(a, b)
+:operator === '-' ? subtract(a, b)
+:operator === '*' ? multiply(a, b)
+:operator === '/' ? divide(a, b): null;
+}
+
+
