89699741116f5b04cac4a37174b46b4e2bc91028
Oscar <oscar.bailey95@gmail.com>
2022-08-25 15:14:58 +0100

    made some improvements but realised the calculator only displays one digit at a time...

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
+     
+    let buttons = document.getElementsByClassName('button');
+    let buttonArray = [];
+    let buttonInnertext = [];
+    let displayNumbers;
+    let calculation = [];
+    let operatorVariable = '*';
+    let aV = 4;
+    let bV = 10;
+    let result;
+    
 const add = (a, b) => a + b;
 
 const subtract = (a,b) => a - b;
@@ -8,16 +19,19 @@ const divide = (a, b) => a / b;
 
 const operate = (operator, a, b) => {
     console.log(operator, a, b);
+
 return operator === '+' ? add(a, b)
 :operator === '-' ? subtract(a, b)
 :operator === '*' ? multiply(a, b)
 :operator === '/' ? divide(a, b): null;
 }
 
-const recordValue = (numberOrOperator) => {
-    
-    calculation += Number(numberOrOperator);
+const recordValues = (numberOrOperator) => {
+(isNaN(numberOrOperator) === false) ? calculation.push(Number(numberOrOperator))
+: calculation.push(numberOrOperator)
+
 console.log(calculation);
+
 }
 
     const populate = (numberOrOperator) => {
@@ -33,16 +47,7 @@ displayNumbers.innerHTML = numberOrOperator;
 
 
  {   
-       
-    let buttons = document.getElementsByClassName('button');
-    let buttonArray = [];
-    let buttonInnertext = [];
-    let displayNumbers;
-    let calculation = 0;
-    let operatorVariable = '-';
-    let aV = 8;
-    let bV = 3;
-    
+  
     
 
 
@@ -54,13 +59,12 @@ displayNumbers.innerHTML = numberOrOperator;
     buttonArray.forEach(element => buttonInnertext.push(element.innerText));
    buttonArray.forEach(element => element.addEventListener('click', () => 
     {
-        if (document.getElementById('AC') !== element){
+        if (document.getElementById('AC') !== element && document.getElementById('equals') !== element){
         populate(element.innerHTML);
+        recordValues(element.innerHTML);
     }
-        if (isNaN(element.innerText) === false){
-        recordValue(element.innerHTML);
+     
         
-    }
 
     })); 
 
@@ -68,8 +72,19 @@ displayNumbers.innerHTML = numberOrOperator;
    equalsButton = document.getElementById('equals');
    equalsButton.addEventListener('click', () =>
    {
-   
- return operate(operatorVariable, aV, bV);
+if (result == false){
+    console.log('reloading');
+}    
+if (calculation.length === 3){
+    operatorVariable = calculation[1];
+aV = calculation[0];
+bV = calculation[2];
+result = operate(operatorVariable, aV, bV);
+displayNumber.innerText = result;
+
+
+}
+
 
    });
 
@@ -77,5 +92,7 @@ acButton = document.getElementById('AC');
 acButton.addEventListener('click', () => {
     displayNumber = document.getElementById('displayNumber');
     displayNumber.innerText = '';
+    calculation.splice(0, 100);
+    result = 0;
 });
  };
