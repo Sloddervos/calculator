265ab44c1662d9282ddb14074ca4e049b9854bc4
Oscar <oscar.bailey95@gmail.com>
2022-08-25 09:53:30 +0100

    display pane working

--- calculator.js
+++ calculator.js
@@ -7,6 +7,7 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const operate = (operator, a, b) => {
+    console.log(operator, a, b);
 return operator === '+' ? add(a, b)
 :operator === '-' ? subtract(a, b)
 :operator === '*' ? multiply(a, b)
@@ -20,7 +21,62 @@ return operator === '+' ? add(a, b)
 
 
 
-// let buttonContainer = document.getElementsByClassName(buttonContainer);
-// const button = document.createElement('button');	
+ {   
+       
+    let buttons = document.getElementsByClassName('button');
+    let buttonArray = [];
+    let buttonInnertext = [];
+    let displayNumbers;
+    let calculation = 0;
+    
+
+const recordValue = (numberOrOperator) => {
+    
+    calculation += Number(numberOrOperator);
+console.log(calculation);
+}
+
+    const populate = (numberOrOperator) => {
+displayNumbers = document.getElementById('displayNumber');
+displayNumbers.innerHTML = numberOrOperator;        
+            }
+            
+
+    for (x = 0; x < buttons.length; x++){
+       buttonArray.push(buttons[x]);
+    }
+    buttonArray.forEach(element => buttonInnertext.push(element.innerText));
+   
+    buttonArray.forEach(element => element.addEventListener('click', () => 
+    {   recordValue(element.innerHTML);
+        populate(element.innerHTML);
+    }));
+
+    let operatorVariable = '-';
+    let aV = 8;
+    let bV = 3;
+    
+   equalsButton = document.getElementById('equals');
+   equalsButton.addEventListener('click', () =>
+   {
+   
+ return operate(operatorVariable, aV, bV);
+
+   });
+acButton = document.getElementById('AC');
+acButton.addEventListener('click', () => {
+    displayNumber = document.getElementById('displayNumber');
+    displayNumber.innerText = '';
+}) 
+
+      
+
+
+}
+
+
+
+
+
 
 
